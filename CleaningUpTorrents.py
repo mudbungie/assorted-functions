8b52c11f690e83c251b33550c418ecc180456e0c
mark <mark@spongezipper.mudbungie.local>
2015-08-12 01:43:28 -0700

    Added support for cleaning up empty directories

--- CleaningUpTorrents.py
+++ CleaningUpTorrents.py
@@ -23,23 +23,26 @@ class mediaLibrary:
         print(self.cwd)
     def renameEverything(self, fileName = None, inheritedPath = None):
         # I don't like this implementation, but it's the syntax I saw recommended
+        # I onlt even go down this road because the recursion requires it
         if fileName == None:
             fileName = self.badName
         if inheritedPath == None:
             inheritedPath = self.cwd
-        
+        fullName = inheritedPath + fileName
+        isDir = os.path.isdir(fullName)
+                
         # crawl through any subdirectories and recurse this function on 
         # every file. Because the recursion happens here, the process goes
         # from bottom to top
-        if os.path.isdir(inheritedPath + fileName):
+        if isDir:
             # to preserve filetypes. Makes more sense when you see it used 
             # on actual files. 
-            fileNameRoot = fileName
+            fileNameRoot = fullName
             fileNameExt = ''
-            for child in os.listdir(inheritedPath + fileName):
+            for child in os.listdir(fullName):
                 # recurse, extending the path with the directory that we're
                 # presently working on
-                self.renameEverything(child, inheritedPath + fileName + os.sep)
+                self.renameEverything(child, fullName + os.sep)
         else:
             # preserving filetypes
             fileNameSections = os.path.splitext(fileName)
@@ -47,11 +50,21 @@ class mediaLibrary:
             fileNameExt = fileNameSections[1]
         # to prevent unbound errors
         whatToDo = False
+        # don't ask if it's either the good name or the bad name (we know what to do)
         if fileNameRoot != self.goodName and fileNameRoot != self.badName:
-            whatToDo = input('File: ' + fileName + ' does not match known patterns.' +
+            whatToDo = input('File: ' + fileName + ' does not match known patterns. ' +
                                 'Delete? [y/N/r(ename)]')
             if whatToDo == 'y':
-                os.remove(inheritedPath + fileName)
+                if isDir:
+                    try:
+                        os.rmdir(fullName)
+                    # don't delete empty directories
+                    except OSError:
+                        print('You actually liked some things in that directory. ' +
+                                'Sending you through to review again.')
+                        self.renameEverything(fileName, inheritedPath)
+                else:
+                    os.remove(fullName)
             # if it's exactly the match, or if the user decided to go ahead and 
             # rename anyways
         if fileNameRoot == self.badName or whatToDo == 'r':
